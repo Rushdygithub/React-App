8243957f8e5e050016f5cb078cccd7cd222cbbfb
Rushdy <rushdynaloordeen@gmail.com>
2024-11-30 11:48:26 +0530

    new-changes-2024-11-30

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import react, {useState} from 'react';
 import {Header} from './componants/Header/Header'
-import { CoreConcept } from './componants/CoreConcept/CoreConcept';
-import { CORE_CONCEPTS } from './data';
+import { CoreConcept} from './componants/CoreConcept/CoreConcept';
+import { CORE_CONCEPTS,EXAMPLES } from './data';
 import {ButtonListFunction} from './componants/ButtonList/ButtonList.jsx'
 
 
@@ -9,14 +9,29 @@ import {ButtonListFunction} from './componants/ButtonList/ButtonList.jsx'
 function App() {
   
   // let updatedContent = 'dynamic content' this will not update - app componants not excutes more than one time
-  const [selectedTopic, setSelectedTopic] = useState('Please Click me')
+  const [selectedTopic, setSelectedTopic] = useState('')
 
+  console.log('intal==========>', EXAMPLES[selectedTopic])
 
   function handleSelect(selectedButton) {
     //Console log output
     console.log(selectedTopic)
     setSelectedTopic(selectedButton)
     console.log('data',selectedTopic);
+  }
+
+  let tableContent = <p>Please Select A Topic</p>
+
+  if(selectedTopic) {
+   tableContent =  <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
   }
 
   //To check excution of the react app
@@ -39,12 +54,12 @@ function App() {
         <section id="examples">
           <menu>
               {/* we pass a function value to button  */}
-              <ButtonListFunction onSelect={() => handleSelect('componants')}>Componants</ButtonListFunction>
+              <ButtonListFunction onSelect={() => handleSelect('components')}>Componants</ButtonListFunction>
               <ButtonListFunction onSelect={()=> handleSelect('jsx')}>JSX</ButtonListFunction>
               <ButtonListFunction onSelect={()=> handleSelect('props')}>Props</ButtonListFunction>
               <ButtonListFunction onSelect={()=> handleSelect('state')}>State</ButtonListFunction>
           </menu>
-          {selectedTopic}
+            {tableContent}
         </section>
       </main>
     </div>
